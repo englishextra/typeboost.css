fa6672f28d6b13c44da7e74449f2991c71826f28
englishextra <englishextra@yandex.ru>
2018-12-25 17:04:56 +0300

    switched to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -105,17 +105,88 @@ prettierOptions = {
 
 var stripDebug = require("gulp-strip-debug");
 
-var csslint = require("gulp-csslint");
+/*!
+ * @see {@link https://github.com/sasstools/sass-lint/blob/master/docs/sass-lint.yml}
+ * @see {@link https://codebeautify.org/yaml-to-json-xml-csv}
+ */
+var csslint = require("gulp-sass-lint");
+var csslintOptions;
+var csslintOptions = {
+	"options": {
+		"max-warnings": 50
+	},
+	"rules": {
+		"border-zero": 0,
+		"brace-style": 0,
+		"class-name-format": 0,
+		"clean-import-paths": 0,
+		"empty-args": 0,
+		"empty-line-between-blocks": 0,
+		"extends-before-declarations": 0,
+		"extends-before-mixins": 0,
+		"final-newline": 0,
+		"force-attribute-nesting": 0,
+		"force-element-nesting": 0,
+		"force-pseudo-nesting": 0,
+		"function-name-format": 0,
+		"hex-length": 0,
+		"hex-notation": 0,
+		"indentation": 0,
+		"leading-zero": 0,
+		"mixin-name-format": 0,
+		"mixins-before-declarations": 0,
+		"nesting-depth": 0,
+		"no-color-keywords": 0,
+		"no-color-literals": 0,
+		"no-css-comments": 0,
+		"no-debug": 0,
+		"no-duplicate-properties": 0,
+		"no-empty-rulesets": 0,
+		"no-extends": 0,
+		"no-ids": 0,
+		"no-important": 0,
+		"no-invalid-hex": 0,
+		"no-mergeable-selectors": 0,
+		"no-misspelled-properties": 0,
+		"no-qualifying-elements": 0,
+		"no-trailing-zero": 0,
+		"no-transition-all": 0,
+		"no-url-protocols": 0,
+		"no-vendor-prefixes": 0,
+		"no-warn": 0,
+		"one-declaration-per-line": 0,
+		"placeholder-in-extend": 0,
+		"placeholder-name-format": 0,
+		"property-sort-order": 0,
+		"pseudo-element": 0,
+		"quotes": 0,
+		"shorthand-values": 0,
+		"single-line-per-selector": 0,
+		"space-after-bang": 0,
+		"space-after-colon": 0,
+		"space-after-comma": 0,
+		"space-around-operator": 0,
+		"space-before-bang": 0,
+		"space-before-brace": 0,
+		"space-before-colon": 0,
+		"space-between-parens": 0,
+		"trailing-semicolon": 0,
+		"url-quotes": 0,
+		"variable-for-property": 0,
+		"variable-name-format": 0,
+		"zero-unit": 0
+	}
+};
 
 var options = {
-	libPaths: {
+	libbundle: {
 		scss: "./scss/*.scss",
 		css: "./css"
 	},
 };
 
 gulp.task("compile-css", function () {
-	return gulp.src(options.libPaths.scss)
+	return gulp.src(options.libbundle.scss)
 	.pipe(plumber())
 	.pipe(sourcemaps.init())
 	.pipe(sass({
@@ -125,27 +196,27 @@ gulp.task("compile-css", function () {
 	.pipe(prettier(prettierOptions))
 	/* .pipe(beautify(beautifyOptions)) */
 	.pipe(plumber.stop())
-	.pipe(gulp.dest(options.libPaths.css))
+	.pipe(gulp.dest(options.libbundle.css))
 	.pipe(rename(function (path) {
 			path.basename += ".min";
 		}))
 	.pipe(minifyCss(cleanCssOptions))
 	.pipe(sourcemaps.write("."))
-	.pipe(gulp.dest(options.libPaths.css));
+	.pipe(gulp.dest(options.libbundle.css));
 });
 
-gulp.task("lint-css", function () {
-	return gulp.src(options.libPaths.css)
-	.pipe(csslint())
-	.pipe(csslint.formatter())
-	.pipe(csslint.failFormatter());
+gulp.task("lint-libbundle-css", function () {
+	return gulp.src(options.libbundle.scss)
+	.pipe(csslint(csslintOptions))
+	.pipe(csslint.format())
+	.pipe(csslint.failOnError());
 });
 
 /*!
  * @see {@link https://browsersync.io/docs/gulp}
  */
 gulp.task("browser-sync", gulp.series(gulp.parallel(
-			"lint-css"), function watchChanges() {
+			"lint-libbundle-css"), function watchChanges() {
 
 		browserSync.init({
 			server: "./"
